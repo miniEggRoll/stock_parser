c30f8fce9f021a02786b5f0c41a15f841b00b586
aaron <aaroncheng@intowow.com>
2017-08-24 01:42:14 +0800

    cache history

--- dataSource/HiStock.py
+++ dataSource/HiStock.py
@@ -5,10 +5,12 @@ from datetime import timedelta
 import numpy
 
 from six.moves import urllib
-from util.format import parse_timestamp, graphlink
+from util.format import parse_timestamp, graphlink, to_timestamp
+from util.log import verbose
 
 COOKIER = urllib.request.HTTPCookieProcessor()
 OPENER = urllib.request.build_opener(COOKIER)
+CONTEXT = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 urllib.request.install_opener(OPENER)
 # Headers to fake a user agent
 HEADERS = {
@@ -22,20 +24,54 @@ class HiStock(object):
         self.end_date = end_date
         self.result = []
 
+    def read(self):
+        stock_file = open(".history/histock_" + self.stock_code + ".json")
+        raw = stock_file.read()
+        stock_file.close()
+        return raw
+
+    def write(self, raw):
+        stock_file = open(".history/histock_" + self.stock_code + ".json", "w")
+        stock_file.write(raw)
+        stock_file.close()
+
+    def check_valid(self, raw):
+        date_begin = self.end_date.replace(
+            hour=0, minute=0, second=0, microsecond=0)
+        return raw.find(str(to_timestamp(date_begin) * 1000)) > -1
+
     def pull(self):
+        from_file = True
+        raw = None
+        try:
+            raw = self.read()
+        except EnvironmentError:
+            from_file = False
+            verbose("fail to read", self.stock_code)
+
+        if raw is not None:
+            if not self.check_valid(raw):
+                from_file = False
+
+        if not from_file:
+            raw = self.download()
+            self.write(raw)
+
+        data = json.loads(raw)
+        self.parse(data)
+
+    def download(self):
         req = urllib.request.Request(
             graphlink(self.stock_code), headers=HEADERS)
-        gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)  # Only for gangstars
-        res = urllib.request.urlopen(req, context=gcontext)
+        res = urllib.request.urlopen(req, context=CONTEXT)
         alines = res.read()
 
         start = alines.find("series: [{")
         end = alines.find("</script>", start)
 
         substring = alines[start:end]
-        result = []
         idx = 0
-        while idx < len(alines):
+        while idx < len(substring):
             name_start = substring.find("type", idx)
             name_end = substring.find("\r", name_start)
             data_start = substring.find("data", name_end)
@@ -48,47 +84,49 @@ class HiStock(object):
             name = substring[name_start:name_end]
             name = name[name.find("'") + 1:name.rfind("'")]
             if name == 'candlestick':
-                data = substring[data_start:data_end]
-                data = data[data.find("["):data.rfind("]") + 1]
-                data = json.loads(data)
+                raw = substring[data_start:data_end]
+                raw = raw[raw.find("["):raw.rfind("]") + 1]
+                return raw
+        raise Exception("problem downloading " + self.stock_code)
 
-                if len(data) >= 20:
-                    j = max(20, len(data) - 180)
+    def parse(self, data):
+        result = []
+        if len(data) >= 20:
+            j = max(20, len(data) - 180)
 
-                    while j < len(data):
-                        k = j - 20
-                        elements = []
-                        while k < j:
-                            elements.append(data[k + 1][4])
-                            k += 1
+            while j < len(data):
+                k = j - 20
+                elements = []
+                while k < j:
+                    elements.append(data[k + 1][4])
+                    k += 1
 
-                        mean = numpy.mean(elements)
-                        std = numpy.std(elements)
+                mean = numpy.mean(elements)
+                std = numpy.std(elements)
 
-                        time = data[j][0]
-                        if parse_timestamp(time) > self.end_date + timedelta(days=1):
-                            j += 1
-                            continue
+                time = data[j][0]
+                if parse_timestamp(time) > self.end_date + timedelta(days=1):
+                    j += 1
+                    continue
 
-                        price = data[j][4]
-                        upperbound = mean + 2 * std
-                        lowerbound = mean - 2 * std
-                        percent_b = 0.5
+                price = data[j][4]
+                upperbound = mean + 2 * std
+                lowerbound = mean - 2 * std
+                percent_b = 0.5
 
-                        if upperbound != lowerbound:
-                            percent_b = (price - lowerbound) / \
-                                (upperbound - lowerbound)
+                if upperbound != lowerbound:
+                    percent_b = (price - lowerbound) / \
+                        (upperbound - lowerbound)
 
-                        result.append({
-                            "time": time,
-                            "price": price,
-                            "20MA": mean,
-                            "std": std,
-                            "upperbound": upperbound,
-                            "lowerbound": lowerbound,
-                            "b": percent_b,
-                            "bw": (upperbound - lowerbound) / mean
-                        })
-                        j += 1
-                break
+                result.append({
+                    "time": time,
+                    "price": price,
+                    "20MA": mean,
+                    "std": std,
+                    "upperbound": upperbound,
+                    "lowerbound": lowerbound,
+                    "b": percent_b,
+                    "bw": (upperbound - lowerbound) / mean
+                })
+                j += 1
         self.result = result
--- util/format.py
+++ util/format.py
@@ -7,3 +7,13 @@ def parse_timestamp(timestamp):
 
 def graphlink(code):
     return 'https://histock.tw/stock/tchart.aspx?m=b&no=' + code
+
+
+def to_timestamp(a_date):
+    if a_date.tzinfo:
+        epoch = datetime(1970, 1, 1, tzinfo=pytz.UTC)
+        diff = a_date.astimezone(pytz.UTC) - epoch
+    else:
+        epoch = datetime(1970, 1, 1)
+        diff = a_date - epoch
+    return int(diff.total_seconds())
